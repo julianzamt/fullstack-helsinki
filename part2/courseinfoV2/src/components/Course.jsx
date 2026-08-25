202d54ed75dd9683f9983b19543536aa30082a9d
Julian Zamt <julizamt@gmail.com>
2026-08-25 16:17:43 +0000

    feat: ex 2.2

--- part2/courseinfoV2/src/components/Course.jsx
+++ part2/courseinfoV2/src/components/Course.jsx
@@ -9,21 +9,27 @@ const Content = ({ parts }) => {
   return <ul>{fmtParts}</ul>;
 };
 
-const Part = ({name, exercises}) => (
+const Part = ({ name, exercises }) => (
   <li>
     {name} {exercises}
   </li>
 );
 
-// const Total = (props) => <p>Number of exercises {props.total}</p>;
+const Total = (props) => <p>Number of exercises {props.total}</p>;
+
+const getTotal = (parts) => {
+  const total = parts.reduce((acc, p) => acc + p.exercises, 0);
+  return total;
+};
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
-      
+
       <Content parts={course.parts} />
-      
+
+      <Total total={getTotal(course.parts)} />
     </div>
   );
 };
