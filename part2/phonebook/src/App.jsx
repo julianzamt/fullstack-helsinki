0923d35a26cb1f4a43d52d8a9bd111513a2a8e5a
Julian Zamt <julizamt@gmail.com>
2026-08-26 13:22:32 +0000

    feat: ex 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "55-55-5555555" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleName = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumber = (e) => {
+    setNewNumber(e.target.value);
   };
 
   const isAdded = () => {
@@ -22,23 +29,30 @@ const App = () => {
 
   const handleAdd = (e) => {
     e.preventDefault();
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
     if (!isAdded()) setPersons(persons.concat(newPerson));
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+
       <form>
         <div>
           name: <input onChange={handleName} />
         </div>
+        <div>
+          {" "}
+          number: <input onChange={handleNumber} />
+        </div>
+
         <div>
           <button type="submit" onClick={handleAdd}>
             add
           </button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <Persons persons={persons} />
     </div>
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,10 +1,16 @@
-const Person = ({ name }) => {
-  return <li>{name}</li>;
+const Person = ({ name, number }) => {
+  return (
+    <li>
+      {name} {number}
+    </li>
+  );
 };
 
 const Persons = ({ persons }) => {
-  const numbers = persons.map((p) => <Person key={p.name} name={p.name} />);
-  return <ul>{numbers}</ul>;
+  const ps = persons.map((p) => (
+    <Person key={p.name} name={p.name} number={p.number} />
+  ));
+  return <ul>{ps}</ul>;
 };
 
 export default Persons;
