d64a3b1042bbcca0c21f37f44f37f86089478c38
Julian Zamt <julizamt@gmail.com>
2026-09-14 18:46:55 +0000

    feat: ex 2.20

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -5,18 +5,23 @@ const Weather = ({ city, lat, lon }) => {
   const [weather, setWeather] = useState(null);
 
   useEffect(() => {
-    weatherService.get(lat, lon).then((w) => setWeather(w.data));
+    weatherService.get(lat, lon).then((w) => {
+      console.log(w);
+      setWeather(w);
+    });
   }, [lat, lon]);
 
   if (!weather) return null;
-  console.log({ weather });
 
   return (
     <div>
       <h3>Weather in {city}</h3>
-      <p>Temperature: {weather.temp}°</p>
-      <p>{weather.weather.icon}</p>
-      <p>Wind: {weather.wind} m/s</p>
+      <p>Temperature: {weather.main.temp}°</p>
+      <img
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt={weather.weather[0].description}
+      />
+      <p>Wind: {weather.wind.speed} m/s</p>
     </div>
   );
 };
